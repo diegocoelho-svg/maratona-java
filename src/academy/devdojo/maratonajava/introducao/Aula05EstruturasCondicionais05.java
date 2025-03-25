87e36c4b0a7e77c79f6c63cec66deb114e241659
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-03-25 08:36:08 -0300

    feat: Aula 26 - Estruturas Condicionais pt 07 - Switch exercício

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais05.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula06EstruturasCondicionais05 {
+public class Aula05EstruturasCondicionais05 {
     public static void main(String[] args) {
         // Imprima o dia da semana, considerando 1 como domingo
         byte dia = 7;
