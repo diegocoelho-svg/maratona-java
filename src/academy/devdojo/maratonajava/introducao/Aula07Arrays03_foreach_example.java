e1c238f09fbdacc36dc013d53113377bfb3f1ffc
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-03-27 21:37:35 -0300

    feat: Aula 35 - Arrays pt 04 - Foreach + Exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays03_foreach_example.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays03_foreach_example {
+    public static void main(String[] args) {
+        int[] elementos = {1,2,3,4,5};
+        int soma = 0;
+
+        for (int numeros : elementos) {
+            soma += numeros;
+        }
+        System.out.println("O resultado da soma de cada item do array é: "+soma);
+    }
+}
