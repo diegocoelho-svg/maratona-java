9547e4c12765259680f67211e7d68af6d1740e6e
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-03-27 22:59:24 -0300

    Aula 37 - Arrays Multidimensionais pt 02 - Foreach

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -16,10 +16,17 @@ public class Aula08ArraysMultidimensionais01 {
 
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
             }
         }
-            
+
+        System.out.println("=============================================");
+
+        for (int[] arrBase : dias) {
+            for (int num : arrBase) {
+                System.out.println(num);
+            }
+        }
     }
 }
