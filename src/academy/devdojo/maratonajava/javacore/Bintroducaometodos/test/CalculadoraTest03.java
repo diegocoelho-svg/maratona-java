1aef471f67b187083a93dafcbba4af34a393dfbc
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-08 19:33:44 -0300

    feat: Aula 47 - Orientação Objetos - Métodos pt 04 - Retorno pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -7,5 +7,12 @@ public class CalculadoraTest03 {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.divideDoisNumeros(20, 2);
         System.out.println(result);
+
+        System.out.println(calculadora.divideDoisNumeros02(20,0));
+
+        System.out.println("---------------------------------");
+
+        calculadora.imprimeDivisaoDeDoisNumeros(86,0);
+
     }
 }
